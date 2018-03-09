b4b726bb2bd2551f7866ef1c5a33e06f1e7b932e
venkat <venkat7834@gmail.com>
2018-03-09 11:11:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# apache001
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: apache001
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+
